7b6158ba623964d2d639cd7cdec8bdaa16835235
goresev <goresev@gmail.com>
2016-04-17 11:21:57 +0300

    json

--- poke_script.js
+++ poke_script.js
@@ -1,3 +1,5 @@
-document.getElementsByClassName('test')[0].addEventListener('click', function(){
-  alert('Hello!');
+$.getJSON('http://pokeapi.co/api/v2/pokemon/', function(pokemon){
+  for(var i = 0; i < pokemon.length; i++) {
+    console.log(pokemon[i].name);
+  }
 });
