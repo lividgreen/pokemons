bd2f36eb0e2890d1233fb5d446c106890725a456
goresev <goresev@gmail.com>
2016-04-24 14:11:22 +0300

    first list

--- poke_script.js
+++ poke_script.js
@@ -1,5 +1,7 @@
-$.getJSON('http://pokeapi.co/api/v2/pokemon/', function(pokemon){
-  for(var i = 0; i < pokemon.length; i++) {
-    console.log(pokemon[i].name);
+$.getJSON('https://pokeapi.co/api/v2/pokemon', function(data) {
+  for (var i = 0; i < 12; i++) {
+    $('.pokeDiv').append('<div></div>');
+    var pokeInfo = data.results;
+    $('.pokeDiv > div:last-child').append(pokeInfo[i].name);
   }
 });
