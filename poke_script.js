23f0090e07999a296c63a606e0b18880e44b205d
Goresev <goresev@gmail.com>
2016-05-05 17:38:10 +0300

    v1

--- poke_script.js
+++ poke_script.js
@@ -1,7 +1,15 @@
-$.getJSON('https://pokeapi.co/api/v2/pokemon', function(data) {
-  for (var i = 0; i < 12; i++) {
-    $('.pokeDiv').append('<div></div>');
-    var pokeInfo = data.results;
-    $('.pokeDiv > div:last-child').append(pokeInfo[i].name);
-  }
+$.getJSON('https://pokeapi.co/api/v2/pokemon', function(data){
+  var result = data.results;
+  var resultUrl = result.url;
+  $.getJSON(resultUrl, function(pokeData){
+    var pokeName = pokeData.name;
+    var pokeType = pokeData.types;
+    var pokeFirstAbil = pokeType[1].name;
+    var pokeSecondAbil = pokeType[0].name;
+    var pokeId = pokeData.id;
+    $('.poke-img').css('background-image', url = 'https://pokeapi.co/media/img/' + pokeId + '.png');
+    $('.poke-name').append(pokeName);
+    $('.poke-slot-1').append(pokeFirstAbil);
+    $('.poke-slot-2').append(pokeSecondAbil);
+  });
 });
