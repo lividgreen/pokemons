41a7c0cfcfb5454d93d3723b8fae9be9619ea749
Livid Green <lividgreen@gmail.com>
2016-05-10 16:49:27 +0300

    fix of pokemons

--- poke_script.js
+++ poke_script.js
@@ -1,13 +1,13 @@
-$.getJSON('https://pokeapi.co/api/v2/pokemon', function(data){
+$.getJSON('http://pokeapi.co/api/v2/pokemon', function(data){
   var result = data.results;
-  var resultUrl = result.url;
+  var resultUrl = result[0].url;
   $.getJSON(resultUrl, function(pokeData){
     var pokeName = pokeData.name;
     var pokeType = pokeData.types;
-    var pokeFirstAbil = pokeType[1].name;
-    var pokeSecondAbil = pokeType[0].name;
+    var pokeFirstAbil = pokeType[0].type.name;
+    var pokeSecondAbil = pokeType[1].type.name;
     var pokeId = pokeData.id;
-    $('.poke-img').css('background-image', url = 'https://pokeapi.co/media/img/' + pokeId + '.png');
+    $('.poke-img').css('background-image', 'url(http://pokeapi.co/media/img/' + pokeId + '.png)');
     $('.poke-name').append(pokeName);
     $('.poke-slot-1').append(pokeFirstAbil);
     $('.poke-slot-2').append(pokeSecondAbil);
